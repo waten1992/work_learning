565d9ac99d875f7ec799dbed7fc0f84b198a88e6
waten <waten1992@gmail.com>
2015-07-05 17:59:59 +0800

    redis 学习文档

---
+++ redis_learning/redis 学习文档.c
@@ -0,0 +1,14 @@
+redis 学习文档
+
+1-介绍：
+
+typedef struct redisReply {
+    int type; /* REDIS_REPLY_* */
+    long long integer; /* The integer when type is REDIS_REPLY_INTEGER */
+    int len; /* Length of string */
+    char *str; /* Used for both REDIS_REPLY_ERROR and REDIS_REPLY_STRING */
+    size_t elements; /* number of elements, for REDIS_REPLY_ARRAY */
+    struct redisReply **element; /* elements vector for REDIS_REPLY_ARRAY */
+} redisReply;
+
+	
