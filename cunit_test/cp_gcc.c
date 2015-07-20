3a9e03c78edb6ea74325bc9b4edc969204af1820
pwd006 <pwd006@Debain>
2015-07-20 16:21:57 +0800

    add spin lock

--- cunit_test/cp_gcc.c
+++ cunit_test/cp_gcc.c
@@ -4,10 +4,11 @@
 #include<sys/time.h>
 #include<semaphore.h>
 
-#define		SYNC			3
+#define		SYNC			4
 #define 	pthread_num 	2
 int g_count = 0 ;
 static sem_t mutex ;
+pthread_spinlock_t lock;
 pthread_mutex_t mutex_p = PTHREAD_MUTEX_INITIALIZER;
 extern int cunit_test_main(int loop_time);
 void do_something();
@@ -48,6 +49,12 @@ void * my_thread( void * arg )
 	v(&mutex);
 
 }
+#elif SYNC == 4
+{
+	pthread_spin_lock(&lock);
+	g_count++;
+	pthread_spin_unlock(&lock);
+}
 #else
 {		g_count++;
 //		do_something();
@@ -60,16 +67,24 @@ int cunit_test_main(int loop_time)
 {
 	pthread_t pid[pthread_num];
 	struct timeval t1 , t2 ;
+#if SYNC == 3
 	sem_init(&mutex,0,1);
+#elif SYNC ==4
+	pthread_spin_init(&lock,PTHREAD_PROCESS_PRIVATE);
+#endif 
 	gettimeofday(&t1,NULL);
 	for(int i = 0 ; i< pthread_num ; i++)
 		pthread_create(&pid[i],NULL,my_thread,&loop_time);
 	for(int i = 0 ; i< pthread_num ; i++)
 		pthread_join(pid[i],NULL);
-
 	gettimeofday(&t2,NULL);
 	double t = (t2.tv_sec -t1.tv_sec)*1000 +(t2.tv_usec - t1.tv_usec)/1000;
 	printf("g_count:%d , used: %f ms \n",g_count , t);
+#if SYNC == 3
+	sem_destroy(&mutex);
+#elif SYNC == 4
+	pthread_spin_destroy(&lock);
+#endif
 return g_count ;
 }
 
