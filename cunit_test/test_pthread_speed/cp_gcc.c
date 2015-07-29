b258fad379fc645fc35f073d7b9879fb27da9fac
waten1992 <pwd006@Debain>
2015-07-29 09:22:35 +0800

    update cunit test

--- cunit_test/test_pthread_speed/cp_gcc.c
+++ cunit_test/test_pthread_speed/cp_gcc.c
@@ -61,13 +61,11 @@ void * my_thread( void * arg )
 }
 #else
 {		g_count++;
-//		do_something();
 }
 #endif
 	timespec_get(&t2, TIME_UTC);
 	double t = (t2.tv_sec -t1.tv_sec)*1000000000 +(t2.tv_nsec - t1.tv_nsec);
-//	printf("come from pthread g_count:%d , used: %f ns \n",g_count , t);
-	printf("%d , %f  \n",g_count , t);
+	printf("come from pthread g_count:%d , used: %f ns \n",g_count , t);
 return NULL ;
 }
 
@@ -89,9 +87,7 @@ int cunit_test_main(int loop_time)
 	
 	gettimeofday(&t2,NULL);
 	double t = (t2.tv_sec -t1.tv_sec)*1000000 +(t2.tv_usec - t1.tv_usec);
-//	printf("g_count:%d , used: %f us \n",g_count , t);
-	printf("%d ,  %f us \n",g_count , t);
-
+	printf("g_count:%d , used: %f us \n",g_count , t);
 #if SYNC == 3
 	sem_destroy(&mutex);
 #elif SYNC == 4
@@ -100,9 +96,3 @@ int cunit_test_main(int loop_time)
 return g_count ;
 }
 
-void do_something()
-{
-	int  i = 10 ;
-	while(i--)
-		;
-}
