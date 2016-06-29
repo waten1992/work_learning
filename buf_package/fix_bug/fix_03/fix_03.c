caf2b1ae7ae3d60bd23c8b6169d0c20e94938754
waten <waten1992@gmail.com>
2016-06-29 10:12:41 +0800

    日常的总结
    过了一年没有碰github, 看来是工作太忙了

--- buf_package/fix_bug/fix_03/fix_03.c
+++ buf_package/fix_bug/fix_03/fix_03.c
@@ -13,9 +13,10 @@
 #include<unistd.h>
 #include<pthread.h>
 
-#define  thread_num 6 
-#define  pool_num   8
-typedef struct
+#define  THREAD_NUM 2
+#define PROD_NUM 26
+
+typedef struct buf_pool
 {
     void *  	buf ;
     size_t    	n;
@@ -25,55 +26,43 @@ typedef struct
     sem_t   mutex;  // 
     sem_t   slots; //available slots for producter
     sem_t   items; //available items for customer
-}buf_pool;
+}buf_pool_t;
 
-typedef struct 
-{
-	void * param ; //Array *
-	buf_pool *master_sp; //sp *
-	size_t num ; //array length
-}master_thread_param;
-
-typedef struct
-{
-	buf_pool *child_sp ;
-	void *value ;
-}child_thread_param;
-
-typedef  struct 
+typedef  struct test_ar
 {	
 	int int_type ;
-	char char_type;
-}ts;
+	double double_type;
+}test_ar_t;
 
 void *thread_function(void *arg);
 void *master_thread_function(void *arg);
 void p(sem_t *s);
 void v(sem_t *s);
-buf_pool *  buf_init(int num , size_t size);
-buf_pool *  add(buf_pool *sp ,void * value);
-void 	 *  get(buf_pool *sp , void *value);
-void		release(buf_pool *sp) ;
+buf_pool_t *  buf_init(int num , size_t size);
+void  add(buf_pool_t *sp ,void * value);
+void  *get(buf_pool_t *sp , void *value);
+void  release(buf_pool_t *sp) ;
 
 
-void p(sem_t *s)
+void 
+p(sem_t *s)
 {
 	if(sem_wait(s) < 0)
 		fprintf(stderr,"sem_wait error \n");
 }
 
-void v(sem_t *s)
+void 
+v(sem_t *s)
 {
 	if(sem_post(s) < 0)
 		fprintf(stderr,"sem_post error \n");
 }
 
-
-buf_pool *  buf_init(int num , size_t size)
+buf_pool_t *  
+buf_init(int num , size_t size)
 {
-	
-    buf_pool *sp ; //declare *sp 
-	sp = (buf_pool *)malloc(sizeof(buf_pool));
+    buf_pool_t *sp ; //declare *sp 
+	sp = (buf_pool_t *)malloc(sizeof(buf_pool_t));
     sp->buf =calloc(num,size);
     sp->n = num;
 	sp->size = size ;
@@ -83,13 +72,9 @@ buf_pool *  buf_init(int num , size_t size)
     sem_init(&sp->items,0,0);
     return sp;
 }
-void look_up(buf_pool *sp)
-{
-    printf("sp->num:%d\n",sp->n);
-    printf("sp->front:%d \n",sp->front);
-    printf("sp->rear:%d \n",sp->rear);
-}
-buf_pool * add(buf_pool *sp ,void * value)
+
+void
+add(buf_pool_t *sp ,void * value)
 {
     p(&sp->slots);
     
@@ -97,21 +82,20 @@ buf_pool * add(buf_pool *sp ,void * value)
     	int i =  (sp->rear)%(sp->n);
     	sp->rear++; 
     v(&sp->mutex);
-
-	printf("sp->size=%x , i=%d ,i*sp->size = %x \n",sp->size,i,i*(sp->size));
+	
    	memcpy(((sp->buf)+(i*(sp->size))),value,sp->size);
-	printf("address of ---> %x ,sp->buf = %x \n",(sp->buf+(i*(sp->size))),&sp->buf);
-    
 	v(&sp->items);
-return  sp; 
 }
 
-void release(buf_pool *sp)
+void 
+release(buf_pool_t *sp)
 {
 	free(sp->buf);
 	free(sp);
 }
-void * get(buf_pool *sp , void *value)
+
+void * 
+get(buf_pool_t *sp , void *value)
 {
 	p(&sp->items);
 	
@@ -121,71 +105,41 @@ void * get(buf_pool *sp , void *value)
 	v(&sp->mutex);
 
 	memcpy(value,(sp->buf+(i*(sp->size))),sp->size);
-	printf("----test of value address--->%x  \n",(sp->buf+(i*(sp->size))));
+	printf("----test of value address--->0x%x  \n",(sp->buf+(i*(sp->size))));
 	
 	v(&sp->slots);
 
 return value ;
 }
 
-int main()
+void fprintPt( pthread_t pt) {
+  unsigned char *ptc = (unsigned char*)(void*)(&pt);
+  size_t i = 0;
+  for ( i=0; i< sizeof(pt); i++) {
+    printf( "%02x ", (unsigned)(ptc[i]));
+  }
+  printf(" i= %d \n",i);
+}
+
+int
+main()
 {
-    buf_pool *sp ;
-
-    sp = buf_init(pool_num,sizeof(double));
-   	#if 0  //for test
-	int i = 4 ;
-	sp = add(sp,&i);  
-	look_up(sp);  
-	#endif 
+    buf_pool_t *sp ;
+    sp = buf_init(PROD_NUM,sizeof(test_ar_t));
+	
 	int res  ; 
-	pthread_t tid[thread_num] ; //store to child pthread
+	pthread_t tid[THREAD_NUM] ; //store to child pthread
 	pthread_t master_tid ;
-	#if 0 //int 
-	int test_arry[] = {10,11,12,13};
-	#endif 
+	fprintPt(pthread_self());
 	
-	#if 0
-	double test_arry[] = {10.0,11.0,12.0,13.0};
-	#endif
-
-	
-	#if  1
-	
-	ts  test_arry[4] = {    };
-	char char_arry[] = {'a','b','c','d'};
-	for (int i = 0 ; i< 4 ; i++)
-	{
-		test_arry[i].int_type = i ;
-		test_arry[i].char_type =char_arry[i]; 
- 	}
-	
-	#endif 
-	
-	printf("ts size ->%d , test_arry--size--> %d \n",sizeof(ts),sizeof(test_arry));
-	
-	master_thread_param *mtp;
-	mtp = (master_thread_param *)malloc(sizeof(master_thread_param));//apply memery
-	mtp->master_sp = sp ; //point to buf_pool
-	mtp->param = test_arry; //address of a test_array
-	mtp->num = sizeof(test_arry)/sizeof(test_arry[0]);//array length	
-
-	res = pthread_create(&master_tid,NULL,master_thread_function,mtp);
+	res = pthread_create(&master_tid,NULL,master_thread_function,sp);
 	if (res != 0)
 	{
 		fprintf(stderr,"pthread was create error \n");
 		exit(EXIT_FAILURE);	
     }
-    pthread_join(master_tid,NULL); 
-#if 1 //for test 
-	look_up(sp);
-#endif
-//	double double_test_array[thread_num] ;
-//	memset(double_test_array,0.0,thread_num); 
-//	child_thread_param *ctp ;
-//	ctp->child_sp = sp ;
-//	ctp->value = double_test_array; 
-	for(int i = 0 ;i < thread_num ;i++) //the child thread 
+    
+	for(int i = 0 ;i < THREAD_NUM ;i++) //the child thread 
 	{
 		res = pthread_create(&tid[i],NULL,thread_function,sp);
 		if (res != 0)
@@ -194,70 +148,51 @@ int main()
 			exit(EXIT_FAILURE);	
 		}
 	}
-   
-	for(int i = 0 ;i < thread_num ;i++) //joinable thread 
-   	{
-		res  = pthread_join(tid[i],NULL);
- 		if(res != 0)
-		{
-			fprintf(stderr,"thread join failed \n");
-			exit(EXIT_FAILURE);	
-		}	
-	}
+	printf("master thread : %lu  \n ", pthread_self());
+	sleep(20);
     release(sp); 
-	free(mtp);
+	
 return 0;
 }
-void *thread_function(void *arg)
+
+void *
+thread_function(void *arg)
 {
-	printf("child thread , it is successfull \n");
-	buf_pool *tmp ;
-	tmp = (buf_pool *)arg ; 
+	pthread_detach(pthread_self());
+	buf_pool_t *tmp ;
+	tmp = (buf_pool_t *)arg ; 
     void *result_tmp ;
-	//printf("before !,the front---->%d \n",tmp->front);
-	#if 0 //  for double test    
-	   double result = 0.0 , res ;
-       result_tmp  = get(tmp,&result);
-	   res =* (double *)result_tmp ;
-	   printf("the result -->%f \n",res);
- 	#endif
-
-
-
-	#if 0 //for int test 
-	int res=-1 , result = 0;
-	result_tmp = get(tmp,&result);
-	res = *(int *)result_tmp;
-	printf("the result---->%d ,after ,the front :%d \n",res,tmp->front);
-//	sleep(1);
-	#endif 
-	
-	#if 1 //  for double test    
-	   ts * res, result ;
-       result_tmp  = get(tmp,&result);
-	   res = (ts *)result_tmp ;
-	   printf("the result -->%d , %c \n",res->int_type,res->char_type);
- 	#endif
+	test_ar_t * res, result ;
+	int i  = 0;
+	sleep(1);
+	while (i < PROD_NUM - 12) 
+	{
+		result_tmp  = get(tmp,&result);
+	  	res = (test_ar_t *)result_tmp ;
+	  	printf("the result -->%d , %f , thread id : %lu \n",
+	  			res->int_type,
+				res->double_type,
+				pthread_self());
+		i++;
+	}
+	printf("consumer thread : %lu exit \n ", pthread_self());
 	pthread_exit(0);
 }
 
-void *master_thread_function(void *arg)
+void *
+master_thread_function(void *arg)
 {
-	master_thread_param *tmp ;
-	tmp = (master_thread_param *)arg;
-	for(int i = 0 ;i < tmp->num  ; i++)
+	pthread_detach(pthread_self());
+	
+	buf_pool_t *sp_master;
+	sp_master = (buf_pool_t *)arg;
+	test_ar_t tmp;
+	for(int i = 0 ;i < PROD_NUM  ; i++)
 	{
-		void *index ; //temp variable
-		int cast_int = (int)(tmp->master_sp->size); //cast the void to int
-		int offset =i * cast_int;// count the offset  
-		memcpy(index,(tmp->param+offset),tmp->master_sp->size);
-		#if 0
-		printf("master thread  inster--> %d \n",*(int *)index); 
-		#endif 
-		tmp->master_sp	= add(tmp->master_sp,index); //call the add 
+		tmp.int_type = i;
+		tmp.double_type = (100.0 + i);
+		add(sp_master, &tmp); //call the add 
 	}
-	printf("form master thread num=%d \n",tmp->master_sp->rear);
-
+	printf("producter thread : %lu exit \n ", pthread_self());
 	pthread_exit(0);
-
 }
